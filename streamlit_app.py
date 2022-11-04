5d7bf7473b7d8df098ac8454667c14055b4d6aed
koreva-liubov <liubov.koreva@magine.com>
2022-11-04 22:07:52 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,13 @@ import numpy as np
 import altair as alt
 import pandas as pd
 import streamlit as st
+from datetime import time, datetime
 
 st.header('st.write')
 
 # Example 1
 
-st.write('Hello, *Everyone!* :sunglasses:')
+st.write('Hello, *World!* :sunglasses:')
 
 # Example 2
 
@@ -16,7 +17,7 @@ st.write(1234)
 # Example 3
 
 df = pd.DataFrame({
-     'first column': [0, 0, 0, 0],
+     'first column': [1, 2, 3, 4],
      'second column': [10, 20, 30, 40]
      })
 st.write(df)
@@ -33,3 +34,41 @@ df2 = pd.DataFrame(
 c = alt.Chart(df2).mark_circle().encode(
      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.write(c)
+
+
+st.header('st.slider')
+
+# Example 1
+
+st.subheader('Slider')
+
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
+
+# Example 2
+
+st.subheader('Range slider')
+
+values = st.slider(
+     'Select a range of values',
+     0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
+
+# Example 3
+
+st.subheader('Range time slider')
+
+appointment = st.slider(
+     "Schedule your appointment:",
+     value=(time(11, 30), time(12, 45)))
+st.write("You're scheduled for:", appointment)
+
+# Example 4
+
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30),
+     format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
